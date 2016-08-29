3b5afcd234b587c6d094ec4a5332d66b9858726a
salman <salmanqureshi3029@gmail.com>
2016-08-29 17:09:51 +0500

    update, show & destroy

---
+++ app/controllers/pics_controller.rb
@@ -0,0 +1,41 @@
+class PicsController < ApplicationController
+	before_action :find_pic, only: [:show, :edit, :update, :destroy]
+	def index
+		@pics = Pic.all.order("created_at DESC")
+	end
+	def new
+		@pic = Pic.new	
+	end
+	def create
+		@pic = Pic.new(pic_params)
+		if @pic.save
+			redirect_to @pic, notice: "yes it was posted"
+		else
+			render 'new'
+		end
+	end
+	def show
+		
+	end
+	def edit
+		
+	end
+	def update
+		if @pic.update(pic_params)
+			redirect_to @pic, notice: "cpnrats! pic was updated"
+		else
+			render 'edit'
+		end
+	end
+	def destroy
+		@pic.destroy
+		redirect_to root_path
+	end
+	private
+		def pic_params
+			params.require(:pic).permit(:title, :description)	
+		end
+		def find_pic
+			@pic = Pic.find(params[:id])
+		end
+end
